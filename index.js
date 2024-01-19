80cad9f1d95388327df8087de6dcb8fcf1eafb54
Amanda Meneguini <admcmarshmallow@gmail.com>
2024-01-19 16:38:07 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let oneYearInMonthsWeeksDaysHoursMinutesSeconds = [
+    [12, 52, 365, 8760, 525600, 31536000]
+]
+
+//função que faz a conversão
+function convert(name, multiplier){
+    
+    console.log(`Olá, ${name} você tem: `)
+     
+    months = oneYearInMonthsWeeksDaysHoursMinutesSeconds[0][0] * multiplier
+    console.log(months + " meses")
+
+    weeks = oneYearInMonthsWeeksDaysHoursMinutesSeconds[0][1] * multiplier
+    console.log(weeks + " semanas")
+
+    days = oneYearInMonthsWeeksDaysHoursMinutesSeconds[0][2] * multiplier
+    console.log(days + " dias")
+
+    hours = oneYearInMonthsWeeksDaysHoursMinutesSeconds[0][3] * multiplier
+    console.log(hours + " horas")
+
+    minutes = oneYearInMonthsWeeksDaysHoursMinutesSeconds[0][4] * multiplier
+    console.log(hours + " minutos")
+
+    seconds = oneYearInMonthsWeeksDaysHoursMinutesSeconds[0][4] * multiplier
+    console.log(seconds + " segundos")
+
+    result = [months, weeks, days, hours, minutes, seconds]
+    console.log(result + " de vida")
+
+}
+
+convert("pedro", 19)
+convert("josé", 26)
